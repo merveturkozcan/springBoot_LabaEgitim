76674a1286b585aa63f3905e96057362cf93ac0a
merve türk <mtturk26@gmail.com>
2024-03-02 22:43:15 +0100

    projectInitialized

---
+++ springBootLaba/src/main/java/com/merveturk/springBootLaba/SpringBootLabaApplication.java
@@ -0,0 +1,13 @@
+package com.merveturk.springBootLaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabaApplication.class, args);
+	}
+
+}
---
+++ springBootLaba/src/test/java/com/merveturk/springBootLaba/SpringBootLabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merveturk.springBootLaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
